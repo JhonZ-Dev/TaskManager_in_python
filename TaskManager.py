8c0403976be8834ac8482ee54529d70c5475baae
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-31 22:00:45 -0500

    Update TaskManager.py

--- TaskManager.py
+++ TaskManager.py
@@ -24,3 +24,45 @@ class TaskManager:
         if os.path.isfile(filename):
             with open(filename, 'r') as file:
                 self.tasks = file.read().splitlines()
+if __name__ == "__main__":
+    task_manager = TaskManager()
+    task_file = "tasks.txt"
+
+    # Cargar tareas desde el archivo si existe
+    task_manager.load_tasks_from_file(task_file)
+
+    while True:
+        print("Options:")
+        print("1. Add a task")
+        print("2. Remove a task")
+        print("3. List tasks")
+        print("4. Save tasks to file")
+        print("5. Quit")
+
+        choice = input("Select an option: ")
+
+        if choice == "1":
+            task = input("Enter the task: ")
+            task_manager.add_task(task)
+            print("Task added.")
+
+        elif choice == "2":
+            task = input("Enter the task to remove: ")
+            task_manager.remove_task(task)
+            print("Task removed.")
+
+        elif choice == "3":
+            tasks = task_manager.list_tasks()
+            print("Tasks:")
+            print(tasks)
+
+        elif choice == "4":
+            task_manager.save_tasks_to_file(task_file)
+            print("Tasks saved to file.")
+
+        elif choice == "5":
+            print("Goodbye!")
+            break
+
+        else:
+            print("Invalid option. Please select a valid option.")

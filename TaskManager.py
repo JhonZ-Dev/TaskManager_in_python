0a4a0425abff4e9840f2be34e75a89bf8b892ea5
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-10-31 21:59:29 -0500

    Update TaskManager.py

--- TaskManager.py
+++ TaskManager.py
@@ -3,4 +3,6 @@ class TaskManager:
     """Clase que administra las tareas"""
     def __init__(self):
         self.tasks = []
-        
+    
+    def add_task(self, task):
+        self.tasks.append(task)
